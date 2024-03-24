fc9f3b3dd9f9efd6aa7e685e34a37ce9f93e8ea8
Caique <asapcaique7@gmail.com>
2024-03-24 10:56:01 -0300

    tela de log inicial finalizada

--- src/bank/managment/java/Login.java
+++ src/bank/managment/java/Login.java
@@ -2,58 +2,98 @@ package bank.managment.java;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
-public class Login extends JFrame {
+public class Login extends JFrame implements ActionListener{
+    JLabel l1,l2,l3;
+    JTextField tf1;
+    JPasswordField pf2;
+    JButton b1,b2,b3;
 
-    Login() {
-
-        setTitle("MAQUINA DE CAIXA AUTOMIZADA");
-
-        setLayout(null);
+    Login(){
+        setTitle("MÁQUINA DE CAIXA AUTOMIZADA");
 
         ImageIcon i1 = new ImageIcon(ClassLoader.getSystemResource("icons/logo.jpg"));
         Image i2 = i1.getImage().getScaledInstance(100, 100, Image.SCALE_DEFAULT);
         ImageIcon i3 = new ImageIcon(i2);
-        JLabel label = new JLabel(i3);
-        label.setBounds(70, 10, 100, 100);
-        add(label);
+        JLabel l11 = new JLabel(i3);
+        l11.setBounds(70, 10, 100, 100);
+        add(l11);
 
-        JLabel text = new JLabel("Bem vindo á ATM");
-        text.setFont(new Font("Osward", Font.BOLD, 38));
-        text.setBounds(200, 40, 400, 40);
-        add(text);
+        l1 = new JLabel("BEM VINDO Á AGÊNCIA");
+        l1.setFont(new Font("Osward", Font.BOLD, 38));
+        l1.setBounds(200,40,450,40);
+        add(l1);
 
-        JLabel cardno = new JLabel("Card Number");
-        text.setFont(new Font("Raleway", Font.BOLD, 38));
-        text.setBounds(120, 150, 150, 30);
-        add(cardno);
+        l2 = new JLabel("Numero do Cartão:");
+        l2.setFont(new Font("Raleway", Font.BOLD, 28));
+        l2.setBounds(125,150,375,30);
+        add(l2);
 
-        JTextField cardTextField = new JTextField();
-        cardTextField.setBounds(300, 150, 250, 30);
-        add(cardTextField);
+        tf1 = new JTextField(15);
+        tf1.setBounds(400,150,230,30);
+        tf1.setFont(new Font("Arial", Font.BOLD, 14));
+        add(tf1);
 
-        JLabel pin = new JLabel("PIN:");
-        pin.setFont(new Font("Raleway", Font.BOLD, 38));
-        pin.setBounds(120, 220, 250, 40);
-        add(pin);
+        l3 = new JLabel("PIN:");
+        l3.setFont(new Font("Raleway", Font.BOLD, 28));
+        l3.setBounds(125,220,375,30);
+        add(l3);
 
-        JTextField pinTextField = new JTextField();
-        pinTextField.setBounds(300, 220, 250, 30);
-        add(pinTextField);
+        pf2 = new JPasswordField(15);
+        pf2.setFont(new Font("Arial", Font.BOLD, 14));
+        pf2.setBounds(300,220,230,30);
+        add(pf2);
 
-        JButton login = new JButton("SIGN IN");
-        login.setBounds(300, 300, 100, 30);
-        login.setBackground(Color.WHITE);
-        add(login);
+        b1 = new JButton("SIGN IN");
+        b1.setBackground(Color.BLACK);
+        b1.setForeground(Color.WHITE);
 
-        getContentPane().setBackground(Color.GRAY);
+        b2 = new JButton("LIMPAR");
+        b2.setBackground(Color.BLACK);
+        b2.setForeground(Color.WHITE);
 
-        setSize(800, 460);
+        b3 = new JButton("SIGN UP");
+        b3.setBackground(Color.BLACK);
+        b3.setForeground(Color.WHITE);
+
+        setLayout(null);
+
+        b1.setFont(new Font("Arial", Font.BOLD, 14));
+        b1.setBounds(300,300,100,30);
+        add(b1);
+
+        b2.setFont(new Font("Arial", Font.BOLD, 14));
+        b2.setBounds(430,300,100,30);
+        add(b2);
+
+        b3.setFont(new Font("Arial", Font.BOLD, 14));
+        b3.setBounds(300,350,230,30);
+        add(b3);
+
+        b1.addActionListener(this);
+        b2.addActionListener(this);
+        b3.addActionListener(this);
+
+        getContentPane().setBackground(Color.WHITE);
+
+        setSize(800,480);
+        setLocation(550,200);
         setVisible(true);
-        setLocation(350, 200);
+
     }
 
-    public static void main(String args[]){
+    @Override
+    public void actionPerformed(ActionEvent ae) {
+        if (ae.getSource() == b2) {
+            l2.setText("");
+            l3.setText("");
+        }
+    }
+
+
+    public static void main(String args[]) {
         new Login();
     }
 }
