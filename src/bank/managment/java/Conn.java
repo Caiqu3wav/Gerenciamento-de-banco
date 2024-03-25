8bf9dc78ec83cf771923dfad2d06360bbe7bdc7d
Caique <asapcaique7@gmail.com>
2024-03-25 14:37:05 -0300

    tela de signup e conexão sql feitas

---
+++ src/bank/managment/java/Conn.java
@@ -0,0 +1,17 @@
+package bank.managment.java;
+import java.sql.*;
+public class Conn {
+    Connection c;
+    Statement s;
+    public Conn(){
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            c =DriverManager.getConnection("jdbc:mysql:///bankmanagement","root","password");
+            s =c.createStatement();
+
+
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+    }
+}
